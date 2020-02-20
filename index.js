08df180066da6bce824c6e1e987dc89b57c14e2c
Scott Kao <scottxxx666@gmail.com>
2020-02-20 19:43:06 +0800

    [feat] get all tickets

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose');
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -13,6 +14,8 @@ const typeDefs = gql`
     payment: String!
     note: String
     contactWay: [ContactWay]
+    createdAt: String!
+    updatedAt: String!
   }
 
   type ContactWay {
@@ -25,24 +28,36 @@ const typeDefs = gql`
   }
 `;
 
-const books = [
-  {
-    id: 'AJDJDOIDJW',
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-  },
-  {
-    id: 'asdqwqf',
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
+const Tickets = mongoose.model('Tickets', new mongoose.Schema({
+  area: String,
+  seat: String,
+  number: Number,
+  price: Number,
+  payment: String,
+  note: String,
+  contactWay: [{ platform: String, id: String }],
+  createdAt: Date,
+  updatedAt: Date,
+}));
+
+const getTickets = async () => {
+  const tickets = await Tickets.find();
+  console.log(tickets);
+  return tickets;
+};
 
 const resolvers = {
   Query: {
-    getTickets: () => books,
+    getTickets: getTickets,
   },
 };
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB_NAME}`, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  autoIndex: true,
+});
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
