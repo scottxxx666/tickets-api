b9f60bdedbaf45eb5196768249d2c5680a847c86
Scott Kao <scottxxx666@gmail.com>
2020-03-26 17:04:01 +0800

    [refactor] context and auth

--- src/context.js
+++ src/context.js
@@ -1,14 +1,19 @@
 const { decodeToken } = require('./components/user');
 
 const context = ({ req }) => {
+  return {
+    user: authUser(req),
+  };
+};
+
+const authUser = (req) => {
   const auth = req.get('Authorization');
-  if (auth) {
-    const token = auth.replace('Bearer ', '');
-    const { user } = decodeToken(token);
-    return {
-      user,
-    };
+  if (!auth) {
+    return null;
   }
+  const token = auth.replace('Bearer ', '');
+  const { user } = decodeToken(token);
+  return user;
 };
 
 module.exports = context;
--- src/format-error.js
+++ src/format-error.js
@@ -2,8 +2,15 @@ const { AuthenticationError } = require('apollo-server');
 const DuplicateError = require('./components/user/error/duplicate-error');
 const NotFoundError = require('./components/user/error/not-found-error');
 
+function isAuthError(err) {
+  return err.message.startsWith('Token ') ||
+    err.originalError instanceof DuplicateError ||
+    err.originalError instanceof NotFoundError ||
+    err.message.startsWith('Context');
+}
+
 const formatError = (err) => {
-  if (err.message.startsWith('Token ') || err.originalError instanceof DuplicateError || err.originalError instanceof NotFoundError) {
+  if (isAuthError(err)) {
     return new AuthenticationError(err.message);
   }
   return err;
