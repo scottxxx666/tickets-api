6ecebc344a32909c727a8cb2266f6dc83d0f0ee9
Scott Kao <scottxxx666@gmail.com>
2020-03-31 01:33:04 +0800

    [feat] add mongo db for dev

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const initDB = () => {
   mongoose.set('useFindAndModify', false);
-  mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB_NAME}`, {
+  mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB_NAME}`, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
